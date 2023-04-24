0fb3ae308cdc52b80582b5aa9042e78b83365f1f
MikelAnderson <mikelanderson.s334891@cesurformacion.com>
2023-04-24 20:53:28 +0200

    24/04/2013: update for abstract class Movimiento and methods

--- src/main/java/proyectoPokemon/model/movimiento/MovimientoDeMejora.java
+++ src/main/java/proyectoPokemon/model/movimiento/MovimientoDeMejora.java
@@ -1,11 +1,44 @@
 package proyectoPokemon.model.movimiento;
 
+import proyectoPokemon.model.Pokemon;
+
 public class MovimientoDeMejora extends Movimiento{
     private TipoMejora mejoraAplicada;
     private int turnosDuracion;
 
 
-    public MovimientoDeMejora(String nombre, int costeDeEstamina) {
+    public MovimientoDeMejora(String nombre, int costeDeEstamina, TipoMejora mejoraAplicada, int turnosDuracion) {
         super(nombre, costeDeEstamina);
+        this.mejoraAplicada = mejoraAplicada;
+        this.turnosDuracion = turnosDuracion;
+        setCosteDeEstamina(this.turnosDuracion * 10);
+    }
+
+    public TipoMejora getMejoraAplicada() {
+        return mejoraAplicada;
+    }
+
+    public void setMejoraAplicada(TipoMejora mejoraAplicada) {
+        this.mejoraAplicada = mejoraAplicada;
+    }
+
+    public int getTurnosDuracion() {
+        return turnosDuracion;
+    }
+
+    public void setTurnosDuracion(int turnosDuracion) {
+        this.turnosDuracion = turnosDuracion;
+    }
+
+    //Metodo para aplicar movimineto de mejora
+    public String aplicarMovimentoMejora(Pokemon pokemon){
+        String mensaje = "";
+        if (getCosteDeEstamina() > pokemon.getEstamina()){
+            mensaje = "No hay estamina suficiente.";
+            return mensaje;
+        }else
+            pokemon.setEstamina(pokemon.getEstamina() - this.getCosteDeEstamina());
+        mensaje = this.getMejoraAplicada() + " durante " + this.turnosDuracion + " turnos";
+        return mensaje;
     }
 }
